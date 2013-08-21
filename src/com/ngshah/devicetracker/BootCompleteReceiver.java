1f81ee0f57a9a2815b606701082f3f798061285c
khushnish <khushnishshah@gmail.com>
2013-08-21 22:12:13 +0530

    Update BootCompleteReceiver.java

--- src/com/ngshah/devicetracker/BootCompleteReceiver.java
+++ src/com/ngshah/devicetracker/BootCompleteReceiver.java
@@ -3,11 +3,14 @@ package com.ngshah.devicetracker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class BootCompleteReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		Log.e("DeviceTracker", "onReceive() called");
+		Log.e("DeviceTracker", "Boot Completed");
 		context.startService(new Intent(context, BootCompleteService.class));
 	}
 }
